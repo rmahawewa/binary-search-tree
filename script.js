dca4040e6a5a5fbb5366adde3cadc79672f217d7
rmahawewa <rmahawewa@yahoo.com>
2025-01-09 12:36:31 +0530

    recorrect implementations

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 class Node{
     constructor(data){
         this.data = data;
-        this.left = "";
-        this.right = "";
+        this.left = null;
+        this.right = null;
     }
+}
+
+class Tree{
+    constructor(array, root){
+        this.root = buildTree(array);
+        this.array = array;
+        this.start = 0;
+        this.end = array.length - 1;
+    }
+
+    buildTree(){
+        let array = this.array;
+        let start = this.start;
+        let end = this.end;
+
+        let sorted_array = array.sort();
+
+        if(start > end) return null;
+
+        //find the mid value
+        let mid = start + Math.floor((end - start)/2);
+
+        let root = new Node(array[mid]);
+        root.left = buildTree(array, start, (mid-1));
+        root.right = buildTree(array, (mid+1), end);
+
+        return root;
+
+    }
+
 }
 
